9880399780b5f1b1b70cef6b9354259c3358d9e8
KEITH LIM <63866293+Kweef206@users.noreply.github.com>
2020-07-09 13:11:21 -0700

    first commit

---
+++ bootcamp_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('create', views.create),
+    path('<int:course_id>/delete', views.delete),
+    
+]
+
+
+
